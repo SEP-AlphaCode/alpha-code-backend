23f85868dd797088ce8d882e4fc42192a074d554
mineduck1608 <ducdm160804@gmail.com>
2025-10-15 14:35:55 +0700

    Revert "adding redis"
    This reverts commit 9febb3fefc7f84732813bb314574d37cc8756f36.

--- app/repositories/skill_repository.py
+++ app/repositories/skill_repository.py
@@ -2,7 +2,6 @@ from sqlalchemy.future import select
 from typing import List, Optional
 from app.entities.database import AsyncSessionLocal
 from app.entities.skill import Skill
-from aiocache import cached
 
 async def get_all_skills() -> List[Skill]:
     """Lấy toàn bộ skills"""
@@ -35,8 +34,6 @@ async def delete_skill_by_id(id: str) -> bool:
             return True
         return False
 
-
-@cached(ttl=60 * 10, key_builder=lambda f, robot_model_id: f"skill:{robot_model_id}")
 async def get_skills_by_robot_model_repo(robot_model_id: str):
     async with AsyncSessionLocal() as session:
         result = await session.execute(
